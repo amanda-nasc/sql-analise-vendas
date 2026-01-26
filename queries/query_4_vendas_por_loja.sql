7d04cea7c80a4934a3b242b9230d608c58dd64cc
Amanda Nascimento <amandagn.lo@gmail.com>
2026-01-26 19:29:30 -0300

    Update query_4_vendas_por_loja.sql

--- queries/query_4_vendas_por_loja.sql
+++ queries/query_4_vendas_por_loja.sql
@@ -1,1 +1,13 @@
+-- Query 4 — Lojas que mais venderam
+-- Objetivo: Identificar as lojas com melhor desempenho em vendas, permitindo comparar resultados entre pontos de venda.
 
+SELECT
+    sto.store_name AS loja,
+    COUNT(fun.paid_date) AS "vendas (#)"
+FROM sales.funnel AS fun
+LEFT JOIN sales.stores AS sto
+    ON fun.store_id = sto.store_id
+WHERE fun.paid_date BETWEEN '2021-08-01' AND '2021-08-31' -- Último mês
+GROUP BY loja
+ORDER BY "vendas (#)" DESC
+LIMIT 5
