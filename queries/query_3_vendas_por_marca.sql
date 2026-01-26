5ce81d90556c8f65afd863e012132b5d92e92cbc
Amanda Nascimento <amandagn.lo@gmail.com>
2026-01-26 19:28:08 -0300

    Update query_3_vendas_por_marca.sql

--- queries/query_3_vendas_por_marca.sql
+++ queries/query_3_vendas_por_marca.sql
@@ -1,1 +1,13 @@
+-- Query 3 — Marcas que mais venderam no mês
+-- Objetivo: Analisar quais marcas apresentaram maior volume de vendas no mês, auxiliando na avaliação de performance de produtos.
 
+SELECT
+    pro.brand AS marca,
+    COUNT(fun.paid_date) AS "vendas (#)"
+FROM sales.funnel AS fun
+LEFT JOIN sales.products AS pro
+    ON fun.product_id = pro.product_id
+WHERE fun.paid_date BETWEEN '2021-08-01' AND '2021-08-31' -- Último mês
+GROUP BY marca
+ORDER BY "vendas (#)" DESC
+LIMIT 5
